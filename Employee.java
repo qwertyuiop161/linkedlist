157de797b1eff5c03c23779d203651e8e774c22e
Muhammad Ghauri <idiotmethere@gmail.com>
2026-07-25 16:16:03 -0400

    Linked lsit implementation

---
+++ Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String address;
+    private String location;
+    
+    public Employee(int id, String name, String address, String location) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void print() {
+        System.out.println(id + " " + name + " " + address + " " + location);
+    }
+}
---
+++ EmployeeList.java
@@ -0,0 +1,111 @@
+import static java.lang.System.out;
+public class EmployeeList {
+    private EmployeeNode head;
+
+    public void add(Employee employee) {
+        EmployeeNode employeeNode = new EmployeeNode(employee);
+        if (head == null) {
+            head = employeeNode;
+            return;
+        }
+        EmployeeNode temp = head;
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+        }
+        temp.setNext(employeeNode);
+    }
+
+    public void print() {
+        if (head == null) {
+            return;
+        }
+        EmployeeNode temp = head;
+        while (temp != null) {
+            temp.getEmployee().print();
+            temp = temp.getNext();
+        }
+        out.println();
+    }
+
+    public EmployeeNode removeOld(int id) {
+        if (head == null) {
+            out.println("No employees to remove");
+            return null;
+        }
+        EmployeeNode temp = head;
+        if (head.getEmployee().getId() == id) {
+            head = (head.getNext()!=null) ? head.getNext() : null;
+            return temp;
+        }
+        while (temp.getNext()!=null) {
+            if (temp.getNext().getEmployee().getId()==id) {
+                EmployeeNode n = temp.getNext();
+                temp.setNext(temp.getNext().getNext());
+                return n;
+            }
+            temp = temp.getNext();
+        }
+        out.println("Employee not found");
+        return null;
+    }
+
+    // public EmployeeNode remove(int id) {
+    //     if (head == null) {
+    //         out.println("No employees to remove");
+    //         return null;
+    //     }
+    //     EmployeeNode last = head;
+    //     EmployeeNode curr = head;
+    
+    //     while (temp.getNext()!=null) {
+    //         if (temp.getNext().getEmployee().getId()==id) {
+    //             EmployeeNode n = temp.getNext();
+    //             temp.setNext(temp.getNext().getNext());
+    //             return n;
+    //         }
+    //         temp = temp.getNext();
+    //     }
+    //     out.println("Employee not found");
+    //     return null;
+    // }
+
+    public Employee find(int id) {
+        if (head == null) {
+            out.println("No employees");
+            return null;
+        }
+        EmployeeNode temp = head;
+        while (temp!=null) {
+            if (temp.getEmployee().getId()==id) {
+                return temp.getEmployee();
+            }
+            temp = temp.getNext();
+        }
+        out.println("employee not found");
+        return null;
+    }
+
+    public boolean insertAt(int id, Employee employee) {
+        if (head == null) {
+            out.println("No employees");
+            return false;
+        }
+        EmployeeNode temp = head;
+        EmployeeNode insert = null;
+        while (temp!=null) {
+            if (temp.getEmployee().getId()==id) {
+                insert = temp;
+                break;
+            }
+            temp = temp.getNext();
+        }
+        if (insert==null) {
+            out.println("Employee not found");
+            return false;
+        }
+        EmployeeNode newE = new EmployeeNode(employee);
+        newE.setNext(insert.getNext());
+        insert.setNext(newE);
+        return true;
+    }
+}
